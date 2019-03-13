84bf59006ca7240d0cf2a1e899585116ef90078f
D7703_07 <d7703_07@lulzsec.local>
2019-03-13 15:09:15 +0900

    2nd common

--- ButtonFun01/ViewController.swift
+++ ButtonFun01/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mySubView: UIView!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.yellow
+        
+        print(myLabel.text!)
+        myLabel.text = "hello iphone"
+        
+        mySubView.backgroundColor = UIColor.red
     }
 
 
